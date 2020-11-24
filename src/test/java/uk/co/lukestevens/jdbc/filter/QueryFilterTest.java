8685db9f3bea47f361a26e05fce562f4d439c121
Luke Stevens <lcm.stevens96@gmail.com>
2020-11-24 21:29:05 +0000

    Update QueryFilterTest package

--- src/test/java/uk/co/lukestevens/jdbc/filter/QueryFilterTest.java
+++ src/test/java/uk/co/lukestevens/jdbc/filter/QueryFilterTest.java
@@ -1,4 +1,4 @@
-package uk.co.lukestevens.jdbc;
+package uk.co.lukestevens.jdbc.filter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
